96ce48e366a3977a2dfc671eaf68f8464fcf082d
Travis Wade <travis@entrision.com>
2016-10-14 07:55:20 -0700

    Initial Commit

---
+++ Branvis Bank/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Branvis Bank
+//
+//  Created by Travis Wade on 10/14/16.
+//  Copyright © 2016 Branvis Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Branvis Bank/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Branvis Bank
+//
+//  Created by Travis Wade on 10/14/16.
+//  Copyright © 2016 Branvis Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
