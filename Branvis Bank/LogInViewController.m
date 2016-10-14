6b314a2789762ec42853cdf2be5779de0be95f23
Travis Wade <travis@entrision.com>
2016-10-14 12:00:55 -0700

    log in UI

--- Branvis Bank/LogInViewController.m
+++ Branvis Bank/LogInViewController.m
@@ -6,17 +6,18 @@
 //  Copyright © 2016 Branvis Software. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LogInViewController.h"
 
-@interface ViewController ()
+@interface LogInViewController ()
 
 @end
 
-@implementation ViewController
+@implementation LogInViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.navigationController setNavigationBarHidden:YES];
 }
 
 
